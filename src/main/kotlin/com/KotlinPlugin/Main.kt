68a629f7198d64166d7bec93570f09f657f38ee8
towa0131 <towa.towa0131@icloud.com>
2017-11-19 04:58:25 +0900

    formatted source

--- src/main/kotlin/com/KotlinPlugin/Main.kt
+++ src/main/kotlin/com/KotlinPlugin/Main.kt
@@ -10,16 +10,19 @@ import cn.nukkit.event.EventHandler
 import cn.nukkit.event.player.PlayerMoveEvent
 
 class Main : PluginBase() , Listener {
-    override fun onEnable() {
-        logger.info("§bEnable Kotlin Plugin")
+
+	override fun onEnable() {
+		logger.info("§aEnabled Kotlin Plugin")
 		server.getPluginManager().registerEvents(this, this);
-    }
+	}
+
 	@EventHandler
 	fun onMove(event : PlayerMoveEvent){
 		val player = event.getPlayer()
 		player.sendPopup("§bWorld:§e"+player.getLevel().getFolderName()+"§bX:§e"+player.x.toInt()+"§bY:§e"+player.y.toInt()+"§bZ:§e"+player.z.toInt())
 	}
+
 	override fun onDisable() {
-        logger.info("§cDisable Kotlin Plugin")
-    }
+		logger.info("§cDisabled Kotlin Plugin")
+	}
 }
